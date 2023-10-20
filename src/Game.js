42cff3c32ff8dc0ec5e9872087771557045a342d
Daniil-ops <daniil252000@mail.ru>
2023-10-20 15:12:52 +0300

    fire

--- src/Game.js
+++ src/Game.js
@@ -2,10 +2,10 @@
 // Или можно не импортировать,
 // а передавать все нужные объекты прямо из run.js при инициализации new Game().
 
-const Hero = require("./game-models/Hero");
-const Enemy = require("./game-models/Enemy");
-const Boomerang = require("./game-models/Boomerang");
-const View = require("./View");
+const Hero = require('./game-models/Hero');
+const Enemy = require('./game-models/Enemy');
+const Boomerang = require('./game-models/Boomerang');
+const View = require('./View');
 
 // Основной класс игры.
 // Тут будут все настройки, проверки, запуск.
@@ -19,17 +19,18 @@ class Game {
     this.boomerang = new Boomerang(trackLength);
     this.track = [];
     this.regenerateTrack();
+    this.live = 3;
   }
 
   regenerateTrack() {
     // Сборка всего необходимого (герой, враг(и), оружие)
     // в единую структуру данных
-    this.track = new Array(this.trackLength).fill(" ");
+    this.track = new Array(this.trackLength).fill(' ');
     this.track[this.hero.position] = this.hero.skin;
     this.track[this.enemy.position] = this.enemy.skin;
     if (
-      this.hero.boomerang.position >= 0 &&
-      this.hero.boomerang.position < this.trackLength
+      this.hero.boomerang.position >= 0
+      && this.hero.boomerang.position < this.trackLength
     ) {
       this.track[this.hero.boomerang.position] = this.hero.boomerang.skin;
     }
@@ -38,16 +39,20 @@ class Game {
 
   check() {
     if (this.hero.position === this.enemy.position) {
-      
-      this.hero.die('DEAD');
+      this.live !== 1 ? this.live -= 1 : this.hero.die('DEAD');
     }
-    if (this.hero.position < 0){
-      this.hero.die('FIRE')
+    if (this.hero.position < 0) {
+      if (this.live !== 1) {
+        this.live -= 1;
+        this.hero.position = 5;
+      } else {
+        this.hero.die('FIRE');
+      }
     }
     if (this.hero.boomerang.position === this.enemy.position || this.hero.boomerang.position === this.enemy.position + 1 || this.hero.boomerang.position === this.enemy.position - 1) {
       this.enemy.die();
-      this.enemy = new Enemy({position: this.trackLength})
-      this.view.score += 10
+      this.enemy = new Enemy({ position: this.trackLength });
+      this.view.score += 10;
     }
   }
 
